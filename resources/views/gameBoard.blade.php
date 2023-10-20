8cda2a32439240fe5a2305ea3a0366d30961792c
David Hajdarovic <david.hajdarovic@boxmark.com>
2023-10-20 15:21:45 +0200

    startet to make the game board

---
+++ resources/views/gameBoard.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="containerTable">
+    <div class="a"></div>
+    <div class="b"></div>
+    <div class="c"></div>
+    <div class="d"></div>
+    <div class="e"></div>
+    <div class="f"></div>
+    <div class="g"></div>
+    <div class="h"></div>
+    <div class="i"></div>
+    <div class="j"></div>
+    <div class="k"></div>
+    <div class="l"></div>
+    <div class="aa"></div>
+    <div class="ab"></div>
+    <div class="ac"></div>
+    <div class="ad"></div>
+    <div class="ae"></div>
+    <div class="af"></div>
+    <div class="ag"></div>
+    <div class="ah"></div>
+    <div class="ai"></div>
+    <div class="aj"></div>
+    <div class="ak"></div>
+  </div>
+@endsection
+
